3d140b642fa43ececc4407ddb5b5553a4356382d
carpercl <perezcarrascospa@gmail.com>
2025-05-26 11:38:41 -0400

    Delete src/App.js

--- src/App.js
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'Arial' }}>
-      <h1>Asistente Contable</h1>
-      <p>Tu app ha sido desplegada correctamente en Vercel 🎉</p>
-      <p>Ahora puedes reemplazar este contenido con tu app real.</p>
-    </div>
-  );
-}
